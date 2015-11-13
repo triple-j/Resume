8a7619ec336c6279b48f79a97801256d631d8ac2
Jeremie Jarosh <jeremie@jarosh.org>
2015-11-13 00:12:22 +0000

    Use PHP to Build the Resume from Multiple Files

--- build.php
+++ build.php
@@ -4,14 +4,30 @@ require(__DIR__ . "/vendor/autoload.php");
 define('_MPDF_TEMP_PATH', "./tmp/");
 define('_MPDF_TTFONTDATAPATH', "./tmp/");
 
-$html = file_get_contents(__DIR__ . "/resume.html");
-$filepath = __DIR__ . "/output/resume.pdf";
+$outputDir    = __DIR__ . "/output/";
+$filepathPDF  = $outputDir . "resume.pdf";
+$filepathHTML = $outputDir . "resume.html";
+
+// make output and temporary directories
+if (!is_dir(_MPDF_TEMP_PATH)) {
+    mkdir(_MPDF_TEMP_PATH);
+}
+if (!is_dir($outputDir)) {
+    mkdir($outputDir);
+}
+
+// build resume
+ob_start();
+require(__DIR__ . "/src/resume.php");
+$html = ob_get_clean();
 
 echo "Rendering PDF..." . PHP_EOL;
 
+file_put_contents($filepathHTML, $html);
+
+// render PDF
 $mpdf=new mPDF();
 $mpdf->WriteHTML($html);
-$mpdf->Output($filepath, 'F');
-//exit;
+$mpdf->Output($filepathPDF, 'F');
 
 echo "done." . PHP_EOL;
