98f1c37da196e50d3d9736c833f920b6a6c8ed58
Jeremie Jarosh <jeremie@jarosh.org>
2015-11-24 17:06:09 +0000

    Remove Months from Employment History

--- src/resume.php
+++ src/resume.php
@@ -67,7 +67,7 @@
    <div class="section">
     <div class="item">
      <p>
-      <span class="employ-date">December 2011 - September 2015</span>
+      <span class="employ-date">2011 - 2015</span>
       <span class="employ-name">Ecreativeworks</span>, Plymouth, MN<br />
       <span class="employ-job">Web Developer</span>
      </p>
@@ -85,7 +85,7 @@
 
     <div class="item">
      <p>
-      <span class="employ-date">January 2010 - October 2010</span>
+      <span class="employ-date">2010</span>
       <span class="employ-name">Travelers Country Club #1</span>, Clear Lake, MN<br />
       <span class="employ-job">Web Developer</span>
      </p>
