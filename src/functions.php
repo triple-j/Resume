d0bddb7b96f1a79841d3bdc04f8da129d6dde268
Jeremie Jarosh <jeremie@jarosh.org>
2015-11-24 19:08:09 +0000

    Obfuscate Email Address

--- src/functions.php
+++ src/functions.php
@@ -4,6 +4,86 @@ function alink($link, $prefix="http://") {
 }
 
 function email($link) {
-    $href = "mailto:" . strtolower($link);
-    return "<a href=\"{$href}\">{$link}</a>";
+    return getObfuscatedEmailLink($link);
+}
+
+/**
+ * Alexei Tenitski -- http://stackoverflow.com/a/12592364
+ */
+function getObfuscatedEmailAddress($email)
+{
+    $alwaysEncode = array('.', ':', '@');
+
+    $result = '';
+
+    // Encode string using oct and hex character codes
+    for ($i = 0; $i < strlen($email); $i++)
+    {
+        // Encode 25% of characters including several that always should be encoded
+        if (in_array($email[$i], $alwaysEncode) || mt_rand(1, 100) < 25)
+        {
+            if (mt_rand(0, 1))
+            {
+                $result .= '&#' . ord($email[$i]) . ';';
+            }
+            else
+            {
+                $result .= '&#x' . dechex(ord($email[$i])) . ';';
+            }
+        }
+        else
+        {
+            $result .= $email[$i];
+        }
+    }
+
+    return $result;
+}
+
+/**
+ * Alexei Tenitski -- http://stackoverflow.com/a/12592364
+ */
+function getObfuscatedEmailLink($email, $params = array())
+{
+    if (!is_array($params))
+    {
+        $params = array();
+    }
+
+    // Tell search engines to ignore obfuscated uri
+    if (!isset($params['rel']))
+    {
+        $params['rel'] = 'nofollow';
+    }
+
+    $neverEncode = array('.', '@', '+'); // Don't encode those as not fully supported by IE & Chrome
+
+    $urlEncodedEmail = '';
+    for ($i = 0; $i < strlen($email); $i++)
+    {
+        // Encode 25% of characters
+        if (!in_array($email[$i], $neverEncode) && mt_rand(1, 100) < 25)
+        {
+            $charCode = ord($email[$i]);
+            $urlEncodedEmail .= '%';
+            $urlEncodedEmail .= dechex(($charCode >> 4) & 0xF);
+            $urlEncodedEmail .= dechex($charCode & 0xF);
+        }
+        else
+        {
+            $urlEncodedEmail .= $email[$i];
+        }
+    }
+
+    $obfuscatedEmail = getObfuscatedEmailAddress($email);
+    $obfuscatedEmailUrl = getObfuscatedEmailAddress('mailto:' . $urlEncodedEmail);
+
+    $link = '<a href="' . $obfuscatedEmailUrl . '"';
+    foreach ($params as $param => $value)
+    {
+        $link .= ' ' . $param . '="' . htmlspecialchars($value). '"';
+    }
+    $link .= '>' . $obfuscatedEmail . '</a>';
+
+    return $link;
 }
